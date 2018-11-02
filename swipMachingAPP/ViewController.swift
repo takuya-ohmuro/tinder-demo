cbeb536e76c76c110985dab4fb60b2e1ad306eca
takuya <takuya93210@yahoo.co.jp>
2018-11-02 15:17:29 +0900

    Initial Commit

---
+++ swipMachingAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swipMachingAPP
+//
+//  Created by 大室拓也 on 2018/11/02.
+//  Copyright © 2018年 taku9321. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
